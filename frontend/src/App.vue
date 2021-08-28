cb48e57266ca2e163dd270167c354478085e73a2
NineRogues <alexander.eric.3@gmail.com>
2021-08-28 05:55:57 -0500

    Navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,24 @@
 <template>
+  <element id="nav">
+  <Navbar></Navbar>
+  </element>
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
